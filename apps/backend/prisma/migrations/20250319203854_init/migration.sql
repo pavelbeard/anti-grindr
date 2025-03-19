1bd7f11ab912ebc059a3e72e6b41388d21286ff6
pavelbeard <pavelborodin1995@gmail.com>
2025-03-20 00:59:56 +0100

    I've finally set up Prisma ORM and I've done migration for apps/backend

---
+++ apps/backend/prisma/migrations/20250319203854_init/migration.sql
@@ -0,0 +1,151 @@
+-- CreateEnum
+CREATE TYPE "SYSTEM_ROLE" AS ENUM ('owner', 'moderator', 'user');
+
+-- CreateEnum
+CREATE TYPE "SEXUAL_ROLE" AS ENUM ('top', 'bottom', 'versatile', 'vers_top', 'vers_bottom', 'side');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "role" "SYSTEM_ROLE" NOT NULL DEFAULT 'user',
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Album" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "order" INTEGER NOT NULL,
+    "isPrivate" BOOLEAN NOT NULL DEFAULT false,
+    "profileId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Album_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Picture" (
+    "id" TEXT NOT NULL,
+    "url" TEXT NOT NULL,
+    "order" INTEGER NOT NULL,
+    "albumId" TEXT,
+    "profileId" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Picture_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Gender" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+
+    CONSTRAINT "Gender_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Pronoun" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+
+    CONSTRAINT "Pronoun_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Profile" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "age" INTEGER,
+    "name" TEXT,
+    "height" DOUBLE PRECISION,
+    "weight" DOUBLE PRECISION,
+    "sexRole" "SEXUAL_ROLE",
+    "bio" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Profile_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "_GenderToProfile" (
+    "A" INTEGER NOT NULL,
+    "B" TEXT NOT NULL,
+
+    CONSTRAINT "_GenderToProfile_AB_pkey" PRIMARY KEY ("A","B")
+);
+
+-- CreateTable
+CREATE TABLE "_ProfileToPronoun" (
+    "A" TEXT NOT NULL,
+    "B" INTEGER NOT NULL,
+
+    CONSTRAINT "_ProfileToPronoun_AB_pkey" PRIMARY KEY ("A","B")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE INDEX "User_email_idx" ON "User"("email");
+
+-- CreateIndex
+CREATE INDEX "Album_createdAt_idx" ON "Album"("createdAt");
+
+-- CreateIndex
+CREATE INDEX "Album_updatedAt_idx" ON "Album"("updatedAt");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Album_profileId_order_key" ON "Album"("profileId", "order");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Picture_albumId_order_key" ON "Picture"("albumId", "order");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Gender_name_key" ON "Gender"("name");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Pronoun_name_key" ON "Pronoun"("name");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Profile_userId_key" ON "Profile"("userId");
+
+-- CreateIndex
+CREATE INDEX "Profile_createdAt_idx" ON "Profile"("createdAt");
+
+-- CreateIndex
+CREATE INDEX "_GenderToProfile_B_index" ON "_GenderToProfile"("B");
+
+-- CreateIndex
+CREATE INDEX "_ProfileToPronoun_B_index" ON "_ProfileToPronoun"("B");
+
+-- AddForeignKey
+ALTER TABLE "Album" ADD CONSTRAINT "Album_profileId_fkey" FOREIGN KEY ("profileId") REFERENCES "Profile"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Picture" ADD CONSTRAINT "Picture_albumId_fkey" FOREIGN KEY ("albumId") REFERENCES "Album"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Picture" ADD CONSTRAINT "Picture_profileId_fkey" FOREIGN KEY ("profileId") REFERENCES "Profile"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Profile" ADD CONSTRAINT "Profile_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_GenderToProfile" ADD CONSTRAINT "_GenderToProfile_A_fkey" FOREIGN KEY ("A") REFERENCES "Gender"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_GenderToProfile" ADD CONSTRAINT "_GenderToProfile_B_fkey" FOREIGN KEY ("B") REFERENCES "Profile"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_ProfileToPronoun" ADD CONSTRAINT "_ProfileToPronoun_A_fkey" FOREIGN KEY ("A") REFERENCES "Profile"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_ProfileToPronoun" ADD CONSTRAINT "_ProfileToPronoun_B_fkey" FOREIGN KEY ("B") REFERENCES "Pronoun"("id") ON DELETE CASCADE ON UPDATE CASCADE;
