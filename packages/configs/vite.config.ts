2418219b3ef5f1aaf727fc7735b7fa0af843cbb7
pavelbeard <pavelborodin1995@gmail.com>
2025-03-19 17:31:12 +0100

    Restored vite.config.ts into apps/web-frontend

--- packages/configs/vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import tailwindcss from "tailwindcss/plugin";
-
-export default defineConfig({
-  // @ts-expect-error: I don't know why this is happening with the `tailwindcss` plugin
-  plugins: [react(), tailwindcss()],
-});
