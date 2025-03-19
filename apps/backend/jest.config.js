baf77f30f78fc3f002af531afe7b1c431105fe55
pavelbeard <pavelborodin1995@gmail.com>
2025-03-20 00:59:01 +0100

    Jest's settings, tsconfig and more

--- apps/backend/jest.config.js
+++ apps/backend/jest.config.js
@@ -1,11 +1,14 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} **/
+
 export default {
   preset: "ts-jest",
   testEnvironment: "node",
   transform: {
-    "^.+\.tsx?$": ["ts-jest", {}],
+    "^.+.tsx?$": ["ts-jest", {}],
   },
   moduleNameMapper: {
     "^@/(.*)$": "<rootDir>/src/$1",
   },
+  // setupFiles: ["<rootDir>/jest.setup.ts"],
+  // setupFilesAfterEnv: ["<rootDir>/singleton.ts"],
 };
