80cd5c3e4cb654148a6bd7fc15fdf9eb7c6dc811
pavelbeard <pavelborodin1995@gmail.com>
2025-03-26 22:08:17 +0100

    .

---
+++ apps/backend/src/types/user.ts
@@ -0,0 +1,31 @@
+import {
+  createUserSchema,
+  signInUserSchema,
+  updateEmailSchema,
+  updatePasswordSchema,
+} from "@/schemas/user.ts";
+import { User } from "@prisma/client";
+import { z } from "zod";
+
+export interface Session {
+  user: {
+    id: string;
+    email: string;
+  } | null;
+}
+
+export type CreateUser = z.infer<typeof createUserSchema>;
+
+export interface PublicUser {
+  id: string;
+  email: string;
+  role: User["role"];
+  createdAt: User["createdAt"];
+  updatedAt: User["updatedAt"];
+}
+
+export type UpdateEmail = z.infer<typeof updateEmailSchema>;
+
+export type UpdatePassword = z.infer<typeof updatePasswordSchema>;
+
+export type SignInUser = z.infer<typeof signInUserSchema>;
