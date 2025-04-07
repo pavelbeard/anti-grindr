3586110189374322c5f02df9229c8f79da185309
pavelbeard <pavelborodin1995@gmail.com>
2025-04-07 21:19:56 +0200

    Changed types for user in apps/backend

--- apps/backend/src/user/user.types.ts
+++ apps/backend/src/user/user.types.ts
@@ -13,12 +13,13 @@ export interface Session {
   userId: string | null
 }
 
-export type CreateUserSchema = z.infer<typeof CreateUserSchema>
-
 export type PublicUser = Omit<User, 'password' | 'refreshToken' | 'role'>
 
+export type CreateUserSchema = z.infer<typeof CreateUserSchema>['body']
+export type SignInUserSchema = z.infer<typeof SignInUserSchema>['body']
+
+export type RefreshTokenSchema = z.infer<typeof RefreshTokenSchema>['body']
 export type UpdateEmailSchema = z.infer<typeof UpdateEmailSchema>
 export type UpdatePasswordSchema = z.infer<typeof UpdatePasswordSchema>
-export type SignInUserSchema = z.infer<typeof SignInUserSchema>
-export type DeleteAccountSchema = z.infer<typeof DeleteAccountSchema>
-export type RefreshTokenSchema = z.infer<typeof RefreshTokenSchema>
+
+export type DeleteAccountSchema = z.infer<typeof DeleteAccountSchema>['body']
