e0a5556bb0b0f1463327c788d7cead78d639b028
pavelbeard <pavelborodin1995@gmail.com>
2025-04-07 21:19:39 +0200

    Added authorization middleware for user.router in apps/backend

--- apps/backend/src/user/user.router.ts
+++ apps/backend/src/user/user.router.ts
@@ -1,4 +1,4 @@
-import { validate } from '@/lib/middlewares.ts'
+import { authorization, validate } from '@/lib/middlewares.ts'
 import * as AuthController from '@/user/user.controller.ts'
 import {
   CreateUserSchema,
@@ -15,28 +15,31 @@ const authRouter: Router = Router()
 
 authRouter.post('/sign-up', validate(CreateUserSchema), AuthController.signUp)
 authRouter.post('/sign-in', validate(SignInUserSchema), AuthController.signIn)
-authRouter.post('/sign-out', AuthController.signOut)
+authRouter.post('/sign-out', authorization, AuthController.signOut)
 authRouter.post(
   '/refresh-token',
   validate(RefreshTokenSchema),
   AuthController.refreshToken
 )
 
-authRouter.get('/:id', AuthController.getUser)
+authRouter.get('/:id', authorization, AuthController.getUser)
 
 authRouter.patch(
   '/:id/update-email',
+  authorization,
   validate(UpdateEmailSchema),
   AuthController.updateEmail
 )
 authRouter.patch(
   '/:id/update-password',
+  authorization,
   validate(UpdatePasswordSchema as unknown as AnyZodObject),
   AuthController.updatePassword
 )
 
 authRouter.delete(
   '/:id',
+  authorization,
   validate(DeleteAccountSchema),
   AuthController.deleteAccount
 )
