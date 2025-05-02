abfbf3ac112a7ebf70555356b4da4c60bd20de85
pavelbeard <pavelborodin1995@gmail.com>
2025-05-02 20:21:03 +0200

    feat: add TanstackQueryDevTools component for development environment

---
+++ apps/frontend/src/TanstackQueryDevTools.tsx
@@ -0,0 +1,20 @@
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { useState } from 'react'
+
+export default function TanstackQueryDevTools() {
+  const [isOpenDevTools, setIsOpenDevTools] = useState(false)
+
+  if (process.env.NODE_ENV === 'development') {
+    return (
+      <section className="absolute bottom-0 left-0 right-0 z-50">
+        <button
+          className="border !px-4 !py-2 rounded-2xl"
+          onClick={() => setIsOpenDevTools(!isOpenDevTools)}
+        >
+          {isOpenDevTools ? 'Hide' : 'Show'} React Query DevTools
+        </button>
+        {isOpenDevTools && <ReactQueryDevtools />}
+      </section>
+    )
+  }
+}
